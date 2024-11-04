a4bb9934e78ad7a436b8c77b1dacdd0ed5beb31e
Tukusej’s Sirs <tukusejssirs@protonmail.com>
2024-11-04 20:59:08 +0100

    refactor: improve `scripts/create-hash.sh`
    The new version uses `useradd` instead of `adduser`, Bash instead of `sh`.
    
    The script always copies over `smb.conf`, as when Samba is installed on Ubuntu, the default configuration is created, but this script fails to use it successfully.
    
    I also removed unnecessary commands.

--- scripts/create-hash.sh
+++ scripts/create-hash.sh
@@ -1,26 +1,22 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# replace original smb config
-grep '# This is the main Samba configuration file.' /etc/samba/smb.conf 2>/dev/null >/dev/null && cp /container/config/samba/smb.conf /etc/samba/smb.conf
+set -euo pipefail
 
-echo -n '>> Enter username: ' 1>&2
-read USERNAME
-echo -n '>> New password: ' 1>&2
-read -s PASSWORD_1
-echo
-echo -n '>> Retype password: ' 1>&2
-read -s PASSWORD_2
-echo
+# Replace original smb config
+cp /container/config/samba/smb.conf /etc/samba/smb.conf
 
-USERNAME=$(echo "$USERNAME" | tr '[:upper:]' '[:lower:]')
+read -rp '>> Enter username: ' USERNAME
+read -srp '>> New password: ' PASSWORD_1
+read -srp "$(printf '\n>> Retype password: ')" PASSWORD_2
 
-if [ "$PASSWORD_1" == "$PASSWORD_2" ] && [ "$PASSWORD_1" != "" ] && [ "$USERNAME" != "" ]
-then
-  adduser -D -H -s /bin/false "$USERNAME" 2> /dev/null >/dev/null
-  smbpasswd -a -n "$USERNAME" 2> /dev/null >/dev/null
-  echo -e "$PASSWORD_1\n$PASSWORD_1" | passwd "$USERNAME" 2> /dev/null >/dev/null
-  echo -e "$PASSWORD_1\n$PASSWORD_1" | smbpasswd "$USERNAME" 2> /dev/null >/dev/null
-  cat /var/lib/samba/private/smbpasswd | grep ':$' | grep '^'"$USERNAME"':[0-9]*:'
+USERNAME=$(tr '[:upper:]' '[:lower:]' <<< "$USERNAME")
+
+if [ "$PASSWORD_1" == "$PASSWORD_2" ] && [ "$PASSWORD_1" != '' ]; then
+  useradd -Ms /bin/false "$USERNAME" &> /dev/null
+  smbpasswd -an "$USERNAME" &> /dev/null
+  usermod --password "$PASSWORD_1" "$USERNAME" &> /dev/null
+  printf "$PASSWORD_1\n$PASSWORD_1\n" | smbpasswd -s "$USERNAME" &> /dev/null
+  grep "^$USERNAME:[0-9]*:.*:$" /var/lib/samba/private/smbpasswd
   exit 0
 fi
 
